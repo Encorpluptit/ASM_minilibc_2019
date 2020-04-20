a89f671235f880b09999880ecd2facef0a9eb437
BERNARD Damien <damien.bernard@epitech.eu>
2020-04-20 15:22:25 +0200

    [FIX] Fixing asm compilation with cmakelist.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ASM_minilibc_2019
+
+
+Doxygen Documentation
+https://stackoverflow.com/questions/267143/doxygen-and-assembly-language
+http://doxygen.10944.n7.nabble.com/Assembly-Language-td896.html
+https://github.com/rfoos/doxygen
---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** test main
+** File description:
+** test main
+*/
+
+#include <inttypes.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include "libasm.h"
+
+int main() {
+    return strlen("lol");
+}
